4e2763b5cd1d352dee9ee735c16e255fc5952a90
Sophie McCarrell <507134+funkjunky@users.noreply.github.com>
2023-04-23 17:41:18 -0400

    Added windows style borders

--- Minesweeper/Square.jsx
+++ Minesweeper/Square.jsx
@@ -2,8 +2,23 @@ import { Pressable, Text } from 'react-native';
 
 const Square = ({ square: { hasMine, minesAround, cleared }, clearSquare }) => {
   const title = hasMine ? 'x' : minesAround;
+  let style = {
+    flexDirection: 'row',
+    backgroundColor: '#DDD',
+    border: 'solid 2px #DDD',
+  };
+  if (!cleared) {
+    style = {
+      ...style,
+      border: undefined,
+      borderLeft: 'solid 2px #FFF',
+      borderTop: 'solid 2px #FFF',
+      borderRight: 'solid 2px #888',
+      borderBottom: 'solid 2px #888',
+    };
+  }
   return (
-    <Pressable onPress={clearSquare} style={{flexDirection: 'row', backgroundColor: 'gray', border: 'solid 2px black'}}>
+    <Pressable onPress={clearSquare} style={style}>
       <Text style={{fontFamily: 'monospace'}}>{cleared ? title : ' '}</Text>
     </Pressable>
   );
